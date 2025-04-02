ddca71766441939b2f6ab8f6c6ca198c75093088
carmen-chau <carmenjchau@gmail.com>
2025-04-01 23:27:20 -0400

    modified interface to add visible attribute

--- components/GraphEdge.tsx
+++ components/GraphEdge.tsx
@@ -7,12 +7,13 @@ interface GraphEdgeInterface {
   source: string;
   target: string;
   label?: string;
+  visible?: boolean; //Denotes whether the edge should be currently visible upon rendering the graph or not
   classes?: string; // Class for styling
 }
 
-const GraphEdge = ({ id, source, target, label, classes}: GraphEdgeInterface) => {
+const GraphEdge = ({ id, source, target, label = "", visible = true, classes}: GraphEdgeInterface) => {
   return {
-    data: { id, source, target, label},
+    data: { id, source, target, label, visible},
     classes, // Assign a class for styling (used to apply a different styling class for forward edges vs backward edges)
   };
 };
